85be2c592d07cd16736190af8d5d4b9c88c3a2ce
Ezequiel <curbina@fi.uba.ar>
2024-06-03 16:42:27 -0300

    agregar errorhandler 404

--- pagina web/savory/app.py
+++ pagina web/savory/app.py
@@ -19,6 +19,10 @@ def services():
 def contact():
     return render_template('contact.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
