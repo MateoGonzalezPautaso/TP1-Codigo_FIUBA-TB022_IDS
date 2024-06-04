a344820f2084166149285cfda440e0858638db75
Ignacio Verruno <iverruno@fi.uba.ar>
2024-06-04 18:08:53 -0300

    feat: agregar formato contact
    adaptar la solapa contact para ser la solapa de inicio de sesión
    
    CARD ID: 665249b37f713570c213d6f3

--- pagina web/savory/app.py
+++ pagina web/savory/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -20,5 +20,5 @@ def contact():
     return render_template('contact.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+	app.run("127.0.0.1", port="5000", debug=True)
