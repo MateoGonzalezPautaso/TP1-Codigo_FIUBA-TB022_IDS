1f144167d26e7ecc8ef8a1d5265e8fb441b8e8d9
Agustin Bermudez <bermudezagustin1201@gmail.com>
2024-06-03 22:30:27 -0300

    feat: se modulariza el codigo html usando estructura block
    CARD ID: 6652495f34d80f4d9dbae3cc

--- pagina web/savory/app.py
+++ pagina web/savory/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
+import requests
 
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -24,5 +25,6 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", port="8080", debug=True)
