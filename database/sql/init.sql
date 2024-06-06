e22a51699dbeaa42689cb022891eafc04e2d269f
Alejandro Mendez <alemendez1223@gmail.com>
2024-06-06 14:25:09 -0300

    feat: asignar valores a las tablas
    se asignaron valores para las tablas de la base de datos
    
    CARD ID: 66577ddda4a9bab49fe581c5

--- database/sql/init.sql
+++ database/sql/init.sql
@@ -15,3 +15,44 @@ CREATE TABLE IF NOT EXISTS recetas(
     PRIMARY KEY(nombre),
     FOREIGN KEY (duenio) REFERENCES usuarios(username)
     );
+
+/* SE INSERTAN VALORES */
+
+INSERT INTO usuarios (username, password) VALUES
+    ('main', '1234');
+
+INSERT INTO recetas (nombre, ingredientes, duenio, descripcion) VALUES
+    (
+        'Spaghetti con salsa de tomate', 
+        '{"spaghetti": "200g", "tomate": "2", "ajo": "2 dientes", "aceite de oliva": "30ml", "sal": "10mg"}', 
+        'main', 
+        'Fideos con salsa de tomate fresca con aceite de oliva y ajo.'
+    ),
+
+    (
+        'Sopa de Tomate', 
+        '{"tomates": "4", "caldo de pollo": "500ml", "cebolla": "1", "ajo": "2 dientes", "aceite de oliva": "30ml", "sal": "5g", "pimienta": "2g", "crema": "100ml"}', 
+        'main', 
+        'Sopa caliente de tomates frescos con cebolla, ajo, caldo de pollo y un toque de crema.'
+    ),
+
+    (
+        'Hamburguesa', 
+        '{"pan de hamburguesa": "1", "carne de res": "200g", "queso cheddar": "1 rebanada", "lechuga": "1 hoja", "tomate": "2 rodajas", "cebolla": "1 rodaja"}', 
+        'main', 
+        'Hamburguesa con carne de res, queso cheddar, lechuga, tomate y cebolla.'
+    ),
+
+    (
+        'Churrasco', 
+        '{"carne de res": "1 kg", "ajo": "3 dientes", "sal": "5g"}', 
+        'main', 
+        'Churrasco a la plancha con ajo.'
+    ),
+
+    (
+        'Brownies de Chocolate', 
+        '{"chocolate": "200g", "manteca": "100g", "azúcar": "200g", "harina": "100g", "huevos": "3", "esencia de vainilla": "5ml"}', 
+        'main', 
+        'Brownies de chocolate con una textura suave y húmeda.'
+    );
