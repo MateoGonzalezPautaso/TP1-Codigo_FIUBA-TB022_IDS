f05782588c08defdcf2253039d127dcb95bedacc
Ignacio Verruno <iverruno@fi.uba.ar>
2024-06-08 20:47:26 -0300

    feat: Adaptar el app.py y el api.py para que trabajen en conjunto
    Adapta app.py para manejar las vistas de los formularios de las sugerencias, a demas de trabajar los datos obtenidos
    
    ID: 66577ee0f84c65dd81a63167

--- webpage/savory/app.py
+++ webpage/savory/app.py
@@ -42,14 +42,32 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/suggest/<username>')
+@app.route('/suggest/<username>', methods=["GET","POST"])
 def suggest(username):
     if not session.get('auth'):           # Si la sesion no esta autenticada, lo devuelve al login
         return redirect(url_for('login'))
-
-    return f'Acceso exitoso. Bienvenido {username}'
+    if request.method == "POST":         #Si es metodo post(ya se completo el primer form), guarda la cantidad de campos a usar
+        cantidad = int(request.form.get("numIngredientes"))
+        nombre = int(request.form.get("namePlato"))      #Estos 2 datos tienen que ser enviados a la api pra meterlos en la BBDD
+        descripcion = int(request.form.get("descPlato"))
+        return redirect(url_for('suggest_ingredientes', cantidad=cantidad, _external=True))    #Redirecciona a el forms de ingredientes, pasando la cantidad de campos
+        
+    return render_template("form_receta.html")
 
     # En esta funcion se va a usar el endpoint POST de la api
+
+
+@app.route('/suggest/ingredientes/', methods=["GET","POST"])
+def suggest_ingredientes():
+    cantidad = int(request.args.get('cantidad'))   #Recibe cantidad como argumento
+    if request.method == "POST":
+        dict_ingredientes = {}   #Crea el diccionario para que puedas ser jsonificado
+        for i in range(cantidad):
+            ingrediente = request.form.get(f"producto{i}")
+            cant = request.form.get(f"cantidad{i}")   #Ingresa los datos en el dict
+            dict_ingredientes[ingrediente] = cant    #ACA HABRIA QUE JSONFICARLO Y QUE LO PUEDAN LLEVAR A LA API
+        return render_template("aceptado.html")
+    return render_template('form_ingredientes.html', cantidad=cantidad)
 
 
 @app.errorhandler(404)
