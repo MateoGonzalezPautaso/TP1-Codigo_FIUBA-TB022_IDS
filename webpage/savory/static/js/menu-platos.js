361637f4230315ada5603ffa8714fd47e0bc712f
Alejandro Mendez <alemendez1223@gmail.com>
2024-06-09 10:31:33 -0300

    feat: agregar funcion para procesar JSON de app.py
    se crea una funcion que toma un JSON proveniente del app.py para poder
    procesarlo y posteriormente enviarselo a la funcion que los muestra en
    el template de menu
    CARD ID:666495b9fb27e7f7aa658854

--- webpage/savory/static/js/menu-platos.js
+++ webpage/savory/static/js/menu-platos.js
@@ -1,26 +1,18 @@
-// Platos hardcodeados
-const platosJson = [
-    {
-        "nombre": "Sopa",
-        "descripcion": "Vor  wieder euch wiederholt widerklang selbst ich tränen, mich wie lebt ihr gut blick gestalten. Und freundschaft gut die."
-    },
-    {
-        "nombre": "Pizza",
-        "descripcion": "De el quedo sus los y el se para, la deja bajo algodón el mudas."
-    },
-    {
-        "nombre": "Churrasco",
-        "descripcion": "Door but bird ungainly ungainly lenore for ember grave. Yore door stood days back and now nevermore, the as if."
-    },
-    {
-        "nombre": "Spaguetti",
-        "descripcion": "Halallal uos leg en scemem hol keguggethuk yg num. Wirud uos fyomnok ezes ezes kyul."
-    }
-];
+// Obtengo el JSON del html
+function obtenerJson() {
+    const container = document.getElementById('contenedor-platos');
+    const platosCadena = container.textContent;  // tipo string
 
-// Declaro la funcion
+    // cuando obtengo el JSON borro el contenido del container
+    container.textContent = '';
+
+    // reemplazo las comillas y casteo a JSON
+    const platosJson = JSON.parse(platosCadena.replaceAll("'", '"'));
+    return platosJson
+}
+
+// Declaro la funcion que muestra los platos
 function mostrarPlatos(platos) {
-    // Obtengo el contenedor de platos
     const container = document.getElementById('contenedor-platos');
 
     // Itero sobre el JSON
@@ -31,13 +23,13 @@ function mostrarPlatos(platos) {
 
         // Creo un <h2>
         const platoNombre = document.createElement('h2');
-        platoNombre.classList.add("fh5co-text")
+        platoNombre.classList.add("fh5co-text");
         // Le agrego el nombre del plato como contenido
         platoNombre.textContent = plato.nombre;
 
         // Creo un <p>
         const platoDescripcion = document.createElement('p');
-        platoDescripcion.classList.add("fh5co-text")
+        platoDescripcion.classList.add("fh5co-text");
         // Le agrego la descripcion del plato como contenido
         platoDescripcion.textContent = plato.descripcion;
 
@@ -50,5 +42,6 @@ function mostrarPlatos(platos) {
     });
 }
 
-// Llamo a la función 
+// Llamo a las funciones
+const platosJson = obtenerJson();
 mostrarPlatos(platosJson);
