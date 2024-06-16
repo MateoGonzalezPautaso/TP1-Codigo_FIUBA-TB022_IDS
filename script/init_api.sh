95724507d34d7d9394ba050cfc5da84bba268f9f
Mateo Gonzalez Pautaso <mateogonzalezpautaso@gmail.com>
2024-06-16 18:21:23 -0300

    refactor: mover ubicaciones archivos
    CARD ID: 666ef6bb8d638404dd9d14c4

--- script/init_api.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pip install mysql-connector-python
-pip install SQLAlchemy==1.4.36
-pip install Flask-SQLAlchemy
--- script/init_proyecto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install python3
-pip install -U pip
-pipenv install Flask
-pipenv shell
