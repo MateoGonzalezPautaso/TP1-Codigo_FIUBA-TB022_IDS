cc1dabeb852decdf87eb73027e5fe2817804d40e
Mateo Gonzalez Pautaso <mateogonzalezpautaso@gmail.com>
2024-06-16 16:28:22 -0300

    feat: modificar scripts para tener todo en uno
    CARD ID: 6667ac0dc8e485ba2d20d9c4

--- script/init_proyecto.sh
+++ script/init_proyecto.sh
@@ -2,4 +2,33 @@
 sudo apt-get install python3
 pip install -U pip
 pipenv install Flask
+
+pip install requests
+
+pip install mysql-connector-python
+pip install SQLAlchemy==1.4.36
+pip install Flask-SQLAlchemy
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+cd .. || exit
+cd database || exit
+cd docker || exit
+docker-compose up --build -d
+cd .. || exit
+cd .. || exit
+
 pipenv shell
